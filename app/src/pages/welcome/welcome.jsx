b12f56f0ac1812a01793cfc163f0cc340fe18aba
sroohandeh <s.roohandeh74@gmail.com>
2022-07-03 15:19:58 +0430

    init

---
+++ app/src/pages/welcome/welcome.jsx
@@ -0,0 +1,42 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from "react-router-dom";
+import ROUTES from "Constants/routes";
+import React from "react";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <section className="section">
+          <div className="container">
+            <section className="hero is-info">
+              <div className="hero-body">
+                <p className="title">
+                  Thank you for trying out the secure-electron-template!
+                </p>
+                <p className="subtitle">
+                  <FontAwesomeIcon icon={faCoffee} />
+                  Please navigate to view the features of this template.
+                </p>
+              </div>
+            </section>
+          </div>
+        </section>
+        <section className="section">
+          <div className="container">
+            <h2 className="title is-2">Samples</h2>
+            <div>
+              <Link to={ROUTES.MOTD}>Using the Electron store.</Link> <br />
+              <Link to={ROUTES.LOCALIZATION}>Changing locales.</Link> <br />
+              <Link to={ROUTES.UNDOREDO}>Undo/redoing actions.</Link> <br />
+              <Link to={ROUTES.CONTEXTMENU}>Custom context menu.</Link> <br />
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Welcome;
